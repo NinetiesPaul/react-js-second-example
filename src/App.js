7b27893b107b7e8fb38226bc47ad65eccd05d0ee
Paul Richard <paulrpm@gmail.com>
2023-01-12 16:28:17 -0300

    handling validation and field msgs

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			<Container component="article" maxWidth="sm">
 				<Typography variant="h3" component="h1" align="center">Formulario de Cadastro</Typography>
-				<RegisterForm onSubmit={onSubmitForm}/>
+				<RegisterForm onSubmit={onSubmitForm} documentValidation={documentValidation}/>
 			</Container>
 		);
 	}
@@ -17,6 +17,14 @@ class App extends Component {
 
 function onSubmitForm(args) {
 	console.log(args);
+}
+
+function documentValidation(cpf) {
+	if (cpf.length !== 11) {
+		return { valid: false, msg: "CPF deve ter 11 dígitos" }
+	}
+
+	return { valid: true, msg: "" }
 }
 
 export default App;
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,12 +1,18 @@
 import { React, useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
-function RegisterForm({onSubmit}) {
+function RegisterForm({onSubmit, documentValidation}) {
     const [name, setName] = useState("");
     const [secondName, setSecondName] = useState("");
     const [cpf, setCpf] = useState("");
     const [promo, setProm] = useState(true);
     const [news, setNewsletter] = useState(true);
+
+    const [errors, setErrors] = useState({
+        cpf: {
+            valid: true, msg: ""
+        }
+    });
 
     return(
         <form onSubmit={
@@ -27,7 +33,22 @@ function RegisterForm({onSubmit}) {
                     setSecondName(event.target.value)
                 }
             }/>
-            <TextField id="cpf" label="CPF" margin="dense" inputProps={{maxLength: 11}} fullWidth
+            <TextField
+            id="cpf"
+            onBlur={
+                (event) => {
+                    const validationCheck = documentValidation(event.target.value);
+                    setErrors({
+                        cpf: validationCheck
+                    })
+                }
+            }
+            error={!errors.cpf.valid}
+            helperText={errors.cpf.msg}
+            label="CPF"
+            margin="dense"
+            //inputProps={{maxLength: 11}}
+            fullWidth
             onChange={
                 (event) => {
                     let tempName = event.target.value;
