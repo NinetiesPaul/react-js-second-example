bf39d7243a05222462bf6b39387bd7309425ebc8
Paul Richard <paulrpm@gmail.com>
2023-01-12 12:17:04 -0300

    using hooks and adding more fields

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import { React, useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 function RegisterForm() {
+    const [name, setName] = useState("");
+    const [secondName, setSecondName] = useState("");
+
     return(
-        <form>
-            <TextField id="nome" label="Nome" margin="dense" fullWidth />
-            <TextField id="sobrenome" label="Sobrenome" margin="dense" fullWidth />
-            <TextField id="cpf" label="CPF" margin="dense" fullWidth />
+        <form onSubmit={
+            (event) => {
+                event.preventDefault();
+                console.log(name, secondName)
+            }
+        }>
+            <TextField id="nome" label="Nome" margin="dense" value={name} fullWidth
+            onChange={
+                (event) => {
+                    let tempName = event.target.value;
+                    if (tempName.length >= 3) {
+                        tempName = tempName.substr(0,3);
+                    }
+                    setName(tempName)
+                }
+            }/>
+            <TextField id="sobrenome" label="Sobrenome" margin="dense"  fullWidth
+            onChange={
+                (event) => {
+                    setSecondName(event.target.value)
+                }
+            }/>
+            <TextField id="cpf" label="CPF" margin="dense" inputProps={{maxLength: 11}} fullWidth />
             <FormControlLabel label="Novidades" control={<Switch name="news" />} />
             <FormControlLabel label="Promoções" control={<Switch name="promo" />} />
             <Button type="submit" variant="contained">Cadastrar</Button>
